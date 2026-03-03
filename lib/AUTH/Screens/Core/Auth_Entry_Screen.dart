19b1c9e122790f0277338bafff6e04f282eeee14
ian <ianmugwe656@gmail.com>
2026-03-03 10:28:22 +0300

    initial commit - auth folder structure

---
+++ lib/AUTH/Screens/Core/Auth_Entry_Screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AuthEntryScreen extends StatelessWidget {
+  const AuthEntryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: const [Colors.blue, Color(0xFFFF8800)],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+
+              // Logo Section
+              Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(),
+                  //Add your logo
+                  child: const Icon(
+                    Icons.person,
+                    size: 200,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
